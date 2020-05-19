204add73715116adeb254dbd5fda97e5cbde18e7
yigangyi <314939403@qq.com>
2020-05-19 15:53:58 +0800

    2020

--- commit.sh
+++ commit.sh
@@ -3,9 +3,11 @@ myip=$(cat myip);
 realip=$(curl icanhazip.com);
 if [ "$myip" = "$realip" ];then
   echo "=="
-else 
-  git commit -m 2020 myip;
+else
+  echo $realip >myip;
   git commit -m 2020 mydns;
+  git commit -m 2020 myip;
+  git commit -m 2020 commit.sh;
   git commit;
   git push;
   cat myip;
